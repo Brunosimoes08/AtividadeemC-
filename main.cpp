4644b41810c9c2492d727a74639f7209c567204b
Brunosimoes08 <cj18101032@gmail.com>
2025-10-23 07:54:27 -0400

    Primeira Atividade

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream> // Substitui <stdio.h> para entrada e saída
+#include <cstdlib>  // Substitui <stdlib.h> para std:system
+
+//
+int main() {
+
+    //
+    std::cout << "Oi! mundo!";
+
+    //
+    //
+    std::cout << "\n";
+    std::system("pause");
+
+    return 0;    
+}
